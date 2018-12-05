1d96461d2c6fdd023932db7e8ca8c6d52b76a90f
ImYrS <i@imyrs.cn>
2018-12-05 12:30:29 +0800

    更新水表bot内容，砍掉ban/active

--- public/config.php
+++ public/config.php
@@ -1,20 +1,20 @@
 <?php
 // Config
 
-$config['key'] = '1145141919810';  // 请求鉴定 Key
+$config['key'] = '1145141919810';	// 请求鉴定 Key
 
-$config['bot_token'] = '';  // Telegram Bot Token
+$config['bot_token'] = '';	// Telegram Bot Token
 
-$config['app_name'] = '';
+$config['app_name'] = '';	//网站名称
 
-$config['db_host'] = 'localhost';
-$config['db_port'] = '3306';
-$config['db_user'] = '';
-$config['db_pass'] = '';
-$config['db_name'] = $config['db_user'];  // 数据库名
+$config['db_host'] = 'localhost';	//数据库地址
+$config['db_port'] = '3306';	//数据库端口
+$config['db_user'] = '';	//数据库用户
+$config['db_pass'] = '';	//数据库密码
+$config['db_name'] = $config['db_user'];	// 数据库名，如果和数据库用户一样就不需要动
 $config['db_char'] = 'utf8';
 
-$config['white_list'] = 'ImYrS23,WooMai';  // 白名单用户
+$config['white_list'] = 'ImYrS23,WooMai';	// 白名单用户
 
 //设置完成后访问以下路径
 //教程： https://imyrs.cn/266.html 
